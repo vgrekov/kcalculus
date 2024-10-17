640dfcfe335267d6fe33da1a0dfb35b824b046e7
Viktor Grekov <viktor.grekov@gmail.com>
2024-10-17 12:14:45 -0400

    Adds riverpod for flutter.

--- kcalculus/lib/main.dart
+++ kcalculus/lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const ProviderScope(
+    child: App(),
+  ));
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
