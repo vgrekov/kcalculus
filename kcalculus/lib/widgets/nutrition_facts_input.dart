9e0d4f2b032a9afae1023ff6fc67aa03de865ebc
Viktor Grekov <viktor.grekov@gmail.com>
2024-10-24 20:43:07 -0400

    Implements nutrition facts input widget.

---
+++ kcalculus/lib/widgets/nutrition_facts_input.dart
@@ -0,0 +1,400 @@
+import 'package:flutter/material.dart';
+import 'package:kcalculus/models/amount.dart';
+import 'package:kcalculus/models/nutrition.dart';
+import 'package:kcalculus/models/units.dart';
+import 'package:kcalculus/utils/messenger.dart';
+import 'package:kcalculus/widgets/amount_input/amount_input.dart';
+
+class NutritionFactsInput extends StatefulWidget {
+  final NutritionFactsInputController? controller;
+  final List<NutritionFacts>? initialValue;
+
+  const NutritionFactsInput({
+    super.key,
+    this.controller,
+    this.initialValue,
+  });
+
+  @override
+  State<StatefulWidget> createState() {
+    return _NutritionFactsInputState();
+  }
+}
+
+class _NutritionFactsInputState extends State<NutritionFactsInput>
+    with StateMessenger {
+  late List<_NutritionFactsDraft> _drafts;
+  int _pageIndex = 0;
+
+  final _form = GlobalKey<FormState>();
+
+  final _pageController = PageController(
+    viewportFraction: 0.8,
+    initialPage: 0,
+  );
+
+  final _perAmountController = AmountInputController();
+  final _caloriesAmountController = AmountInputController();
+  final _fatAmountController = AmountInputController();
+  final _carbsAmountController = AmountInputController();
+  final _fiberAmountController = AmountInputController();
+  final _proteinAmountController = AmountInputController();
+
+  @override
+  void initState() {
+    if (widget.controller != null) {
+      widget.controller!.addListener(_onControllerCommand);
+    }
+
+    if (widget.initialValue != null) {
+      _drafts =
+          widget.initialValue!.map(_NutritionFactsDraft.fromCleanCopy).toList();
+    } else {
+      _drafts = [
+        _NutritionFactsDraft(),
+      ];
+    }
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    widget.controller?.removeListener(_onControllerCommand);
+
+    _perAmountController.dispose();
+    _caloriesAmountController.dispose();
+    _fatAmountController.dispose();
+    _carbsAmountController.dispose();
+    _fiberAmountController.dispose();
+    _proteinAmountController.dispose();
+
+    super.dispose();
+  }
+
+  void _onControllerCommand() {
+    if (widget.controller?._command != null) {
+      switch (widget.controller!._command!) {
+        case _NutritionFactsInputControllerCommand.validate:
+          widget.controller!._isValid = _validate();
+          break;
+        case _NutritionFactsInputControllerCommand.save:
+          _form.currentState!.save();
+          widget.controller!._nutritionFacts =
+              _drafts.map((d) => d.toCleanCopy()).toList();
+          break;
+      }
+    }
+  }
+
+  bool _validate() {
+    var isValid = _form.currentState!.validate();
+
+    if (isValid) {
+      _form.currentState!.save();
+      final index =
+          _drafts.indexWhere((d) => d.fiberInGrams! > d.carbsInGrams!);
+      if (index != -1) {
+        isValid = false;
+        if (_pageIndex != index) {
+          _pageController.animateToPage(
+            index,
+            duration: const Duration(milliseconds: 200),
+            curve: Curves.linear,
+          );
+        }
+        showNotification('There can not be more fiber than carbs.');
+      }
+    }
+
+    return isValid;
+  }
+
+  void _onPageChanged(int index) {
+    if (_pageIndex != index) {
+      if (_form.currentState!.validate()) {
+        _form.currentState!.save();
+
+        if (index < _drafts.length) {
+          _selectDraft(_drafts[index]);
+        }
+
+        setState(() {
+          _pageIndex = index;
+        });
+      } else {
+        _pageController.jumpToPage(_pageIndex);
+      }
+    }
+  }
+
+  void _addPage() {
+    if (_form.currentState!.validate()) {
+      _form.currentState!.save();
+
+      final lastDraft = _drafts[_pageIndex];
+      final draft = _NutritionFactsDraft(
+        calories: lastDraft.calories,
+        fatInGrams: lastDraft.fatInGrams,
+        carbsInGrams: lastDraft.carbsInGrams,
+        fiberInGrams: lastDraft.fiberInGrams,
+        proteinInGrams: lastDraft.proteinInGrams,
+      );
+      _selectDraft(draft);
+
+      setState(() {
+        _drafts.add(draft);
+        _pageIndex++;
+      });
+
+      _pageController.animateToPage(
+        _pageIndex,
+        duration: const Duration(milliseconds: 200),
+        curve: Curves.linear,
+      );
+    }
+  }
+
+  void _deletePage() {
+    final _NutritionFactsDraft draft;
+    final int newIndex;
+    if (_pageIndex < _drafts.length - 1) {
+      draft = _drafts[_pageIndex + 1];
+      newIndex = _pageIndex;
+    } else {
+      draft = _drafts[_pageIndex - 1];
+      newIndex = _pageIndex - 1;
+    }
+    _selectDraft(draft);
+
+    setState(() {
+      _drafts.removeAt(_pageIndex);
+      _pageIndex = newIndex;
+    });
+
+    _pageController.animateToPage(
+      _pageIndex,
+      duration: const Duration(milliseconds: 200),
+      curve: Curves.linear,
+    );
+  }
+
+  void _selectDraft(_NutritionFactsDraft draft) {
+    _perAmountController.setAmount(draft.amount);
+    _caloriesAmountController.setValue(draft.calories);
+    _fatAmountController.setValue(draft.fatInGrams);
+    _carbsAmountController.setValue(draft.carbsInGrams);
+    _fiberAmountController.setValue(draft.fiberInGrams);
+    _proteinAmountController.setValue(draft.proteinInGrams);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _NutritionFactsDraft draft = _drafts[_pageIndex];
+    final isLastPage = _pageIndex == _drafts.length - 1;
+    final isDeletable = _drafts.length > 1;
+    return Form(
+      key: _form,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              IconButton(
+                onPressed: !isDeletable ? null : _deletePage,
+                icon: const Icon(Icons.remove),
+                color: Theme.of(context).colorScheme.primary,
+                iconSize: 24,
+              ),
+              Text(
+                'Nutrition Facts',
+                style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                      color: Theme.of(context).colorScheme.onPrimaryContainer,
+                    ),
+              ),
+              IconButton(
+                onPressed: !isLastPage ? null : _addPage,
+                icon: const Icon(Icons.add),
+                color: Theme.of(context).colorScheme.primary,
+                iconSize: 24,
+              ),
+            ],
+          ),
+          const SizedBox(height: 8),
+          SizedBox(
+            height: 90,
+            child: PageView.builder(
+              controller: _pageController,
+              pageSnapping: true,
+              itemCount: _drafts.length,
+              itemBuilder: (context, index) {
+                final amount = _drafts[index].amount;
+                final isEnabled = index == _pageIndex;
+                return Padding(
+                  key: UniqueKey(),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 24,
+                    vertical: 8,
+                  ),
+                  child: AmountInput(
+                    controller: _perAmountController,
+                    initialAmount: amount,
+                    label: 'Per',
+                    enabled: isEnabled,
+                    onSaveAmount: (amount) {
+                      _drafts[index].amount = amount;
+                    },
+                  ),
+                );
+              },
+              onPageChanged: _onPageChanged,
+            ),
+          ),
+          const SizedBox(height: 16),
+          AmountInput(
+            controller: _caloriesAmountController,
+            label: 'Calories',
+            initialUnit: Unit.calorie,
+            initialValue: draft.calories,
+            fixedUnit: true,
+            onSaveAmount: (amount) {
+              draft.calories = amount?.value;
+            },
+          ),
+          const SizedBox(height: 12),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: AmountInput(
+                  controller: _fatAmountController,
+                  label: 'Fat',
+                  initialUnit: Unit.gram,
+                  initialValue: draft.fatInGrams,
+                  fixedUnit: true,
+                  onSaveAmount: (amount) {
+                    draft.fatInGrams = amount?.value;
+                  },
+                ),
+              ),
+              const SizedBox(width: 8),
+              Expanded(
+                child: AmountInput(
+                  controller: _carbsAmountController,
+                  label: 'Carbs',
+                  initialUnit: Unit.gram,
+                  initialValue: draft.carbsInGrams,
+                  fixedUnit: true,
+                  onSaveAmount: (amount) {
+                    draft.carbsInGrams = amount?.value;
+                  },
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 12),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: AmountInput(
+                  controller: _fiberAmountController,
+                  label: 'Fiber',
+                  initialUnit: Unit.gram,
+                  initialValue: draft.fiberInGrams,
+                  fixedUnit: true,
+                  onSaveAmount: (amount) {
+                    draft.fiberInGrams = amount?.value;
+                  },
+                ),
+              ),
+              const SizedBox(width: 8),
+              Expanded(
+                child: AmountInput(
+                  controller: _proteinAmountController,
+                  label: 'Protein',
+                  initialUnit: Unit.gram,
+                  initialValue: draft.proteinInGrams,
+                  fixedUnit: true,
+                  onSaveAmount: (amount) {
+                    draft.proteinInGrams = amount?.value;
+                  },
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _NutritionFactsDraft {
+  Amount? amount;
+  double? calories;
+  double? fatInGrams;
+  double? carbsInGrams;
+  double? fiberInGrams;
+  double? proteinInGrams;
+
+  _NutritionFactsDraft({
+    this.amount,
+    this.calories,
+    this.fatInGrams,
+    this.carbsInGrams,
+    this.fiberInGrams,
+    this.proteinInGrams,
+  });
+
+  factory _NutritionFactsDraft.fromCleanCopy(NutritionFacts cleanCopy) {
+    return _NutritionFactsDraft(
+      amount: cleanCopy.amount,
+      calories: cleanCopy.nutrientData.calories,
+      fatInGrams: cleanCopy.nutrientData.fatInGrams,
+      carbsInGrams: cleanCopy.nutrientData.carbsInGrams,
+      fiberInGrams: cleanCopy.nutrientData.fiberInGrams,
+      proteinInGrams: cleanCopy.nutrientData.proteinInGrams,
+    );
+  }
+
+  NutritionFacts toCleanCopy() {
+    return NutritionFacts(
+      amount: amount!,
+      nutrientData: NutrientData(
+        calories: calories!,
+        fatInGrams: fatInGrams!,
+        carbsInGrams: carbsInGrams!,
+        fiberInGrams: fiberInGrams!,
+        proteinInGrams: proteinInGrams!,
+      ),
+    );
+  }
+}
+
+enum _NutritionFactsInputControllerCommand {
+  validate,
+  save,
+}
+
+class NutritionFactsInputController extends ChangeNotifier {
+  bool _isValid = false;
+  List<NutritionFacts>? _nutritionFacts;
+
+  _NutritionFactsInputControllerCommand? _command;
+
+  bool get isValid => _isValid;
+
+  List<NutritionFacts>? get nutritionFacts => _nutritionFacts;
+
+  void validate() {
+    _command = _NutritionFactsInputControllerCommand.validate;
+    notifyListeners();
+  }
+
+  void save() {
+    _command = _NutritionFactsInputControllerCommand.save;
+    notifyListeners();
+  }
+}
