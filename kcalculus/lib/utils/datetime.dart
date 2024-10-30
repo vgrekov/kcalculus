5fc8de33962ad8a206dc98018fc5a264591fbd1b
Viktor Grekov <viktor.grekov@gmail.com>
2024-10-30 13:10:53 -0400

    Adds support for ISO8601 formatting and parsing.

--- kcalculus/lib/utils/datetime.dart
+++ kcalculus/lib/utils/datetime.dart
@@ -2,6 +2,7 @@ import 'package:intl/intl.dart';
 
 final _timeFormatter = DateFormat('HH:mm');
 final _dateFormatter = DateFormat('dd-MM-yyyy');
+final _dateTimeFormatISO8601 = DateFormat('yyyy-MM-dd HH:mm:ss.SSS');
 
 String formatTime(DateTime dateTime) {
   return _timeFormatter.format(dateTime);
@@ -9,4 +10,12 @@ String formatTime(DateTime dateTime) {
 
 String formatDate(DateTime dateTime) {
   return _dateFormatter.format(dateTime);
+}
+
+String formatISO8601(DateTime dateTime) {
+  return _dateTimeFormatISO8601.format(dateTime);
+}
+
+DateTime parseISO8601(String str) {
+  return _dateTimeFormatISO8601.parse(str);
 }
