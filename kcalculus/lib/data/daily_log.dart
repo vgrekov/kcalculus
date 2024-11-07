4f7c23c9eff36525b6a20c8ddfe0503d69257fe9
Viktor Grekov <viktor.grekov@gmail.com>
2024-11-07 09:40:19 -0500

    Updates data layer:  - new DAO interfaces: EdibleDao and FoodDao  - search function in EdibleDao  - separate DAO from LocalDAO

--- kcalculus/lib/data/daily_log.dart
+++ kcalculus/lib/data/daily_log.dart
@@ -1,24 +1,24 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:kcalculus/data/local/meal_dao.dart';
+import 'package:kcalculus/data/dao.dart';
 import 'package:kcalculus/data/log_date.dart';
 import 'package:kcalculus/models/meal.dart';
 
 class DailyLogNotifier extends Notifier<Future<List<Meal>>> {
   @override
   Future<List<Meal>> build() async {
-    final mealDao = await ref.watch(localMealDaoProvider);
+    final mealDao = await ref.watch(mealDaoProvider);
     final date = ref.watch(logDateProvider);
     return mealDao.getByDate(date);
   }
 
   Future<void> addMeal(Meal meal) async {
-    final mealDao = await ref.read(localMealDaoProvider);
+    final mealDao = await ref.read(mealDaoProvider);
     await mealDao.add(meal);
     ref.read(logDateProvider.notifier).selectDate(DateTime.now());
   }
 
   Future<bool> deleteMeal(Meal meal) async {
-    final mealDao = await ref.read(localMealDaoProvider);
+    final mealDao = await ref.read(mealDaoProvider);
     final result = await mealDao.delete(meal.id!);
     ref.read(logDateProvider.notifier).selectDate(DateTime.now());
     return result;
--- kcalculus/lib/data/local/food_dao.dart
+++ kcalculus/lib/data/local/food_dao.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kcalculus/data/dao.dart';
 import 'package:kcalculus/data/local/db.dart';
 import 'package:kcalculus/data/local/edible_dao.dart';
 import 'package:kcalculus/data/local/nutrition_facts_dao.dart';
@@ -6,7 +7,7 @@ import 'package:kcalculus/models/food.dart';
 import 'package:kcalculus/utils/ids.dart';
 import 'package:sqflite/sqflite.dart';
 
-class LocalFoodDao {
+class LocalFoodDao implements FoodDao {
   final Database db;
   final LocalEdibleDao edibleDao;
   final LocalNutritionFactsDao nutritionFactsDao;
@@ -35,6 +36,7 @@ class LocalFoodDao {
     );
   }
 
+  @override
   Future<Food?> getById(String id) async {
     return db.rawQuery(
       '''
