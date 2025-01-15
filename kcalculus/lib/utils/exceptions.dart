84357874b77c26826ce91b493b7a14a8a29dc8bc
Viktor Grekov <viktor.grekov@gmail.com>
2025-01-15 18:23:17 -0500

    Adds localized exception class.

--- kcalculus/lib/utils/exceptions.dart
+++ kcalculus/lib/utils/exceptions.dart
@@ -1,3 +1,5 @@
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
 class DuplicationException implements Exception {
   final dynamic message;
 
@@ -22,6 +24,25 @@ class IngredientsCycleException implements Exception {
   String toString() {
     if (message != null) {
       return "IngredientsCycleException: $message";
+    }
+
+    return "IngredientsCycleException";
+  }
+}
+
+class LocalizedException implements Exception {
+  final String Function(AppLocalizations) localizedMessage;
+  final dynamic cause;
+
+  LocalizedException(
+    this.localizedMessage, {
+    this.cause,
+  });
+
+  @override
+  String toString() {
+    if (cause != null) {
+      return "IngredientsCycleException caused by:\n$cause";
     }
 
     return "IngredientsCycleException";
