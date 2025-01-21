244dcc4dcdc58272fbb85828de9608bd08dee09f
Viktor Grekov <viktor.grekov@gmail.com>
2025-01-21 15:16:54 -0500

    Freezes AppSettings model.

---
+++ kcalculus/lib/domain/models/app_settings.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'app_settings.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Map<String, dynamic> _$$AppSettingsImplToJson(_$AppSettingsImpl instance) =>
+    <String, dynamic>{
+      'theme': _$AppThemeEnumMap[instance.theme]!,
+    };
+
+const _$AppThemeEnumMap = {
+  AppTheme.system: 'system',
+  AppTheme.light: 'light',
+  AppTheme.dark: 'dark',
+};
