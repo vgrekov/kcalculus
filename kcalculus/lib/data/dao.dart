9a9b21b044aeb34d15e9dad37de9cfa78819a823
Viktor Grekov <viktor.grekov@gmail.com>
2025-01-28 11:20:24 -0500

    Cleanup

--- kcalculus/lib/data/dao.dart
+++ kcalculus/lib/data/dao.dart
@@ -2,27 +2,9 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kcalculus/data/local/dish_dao.dart';
 import 'package:kcalculus/data/local/edible_dao.dart';
 import 'package:kcalculus/data/local/food_dao.dart';
-import 'package:kcalculus/data/local/meal_dao.dart';
 import 'package:kcalculus/domain/models/dish/dish.dart';
 import 'package:kcalculus/domain/models/edible_search_result.dart';
 import 'package:kcalculus/domain/models/food.dart';
-import 'package:kcalculus/domain/models/meal.dart';
-
-abstract interface class MealDao {
-  Future<void> add(Meal model);
-
-  Future<List<Meal>> getByDate(DateTime date);
-
-  Future<void> update(Meal model);
-
-  Future<bool> delete(String id);
-
-  Future<bool> restore(String id);
-}
-
-final mealDaoProvider = Provider<Future<MealDao>>((ref) {
-  return ref.watch(localMealDaoProvider);
-});
 
 abstract interface class EdibleDao {
   Future<List<EdibleSearchResult>> search(String? query);
