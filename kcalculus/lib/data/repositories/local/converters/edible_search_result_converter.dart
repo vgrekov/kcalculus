f848dfdfe4f926d90a30cfc8bdaa70a763b465b2
Viktor Grekov <viktor.grekov@gmail.com>
2025-01-29 14:41:54 -0500

    Fixes: edible search result domain model's lastEatenAt property not populated.

--- kcalculus/lib/data/repositories/local/converters/edible_search_result_converter.dart
+++ kcalculus/lib/data/repositories/local/converters/edible_search_result_converter.dart
@@ -1,15 +1,18 @@
 import 'package:kcalculus/data/services/local/database/edible/edible_search_result_db_model.dart';
 import 'package:kcalculus/domain/models/edible_search_result.dart';
+import 'package:kcalculus/utils/datetime.dart' as dt;
 
 class LocalEdibleSearchResultConverter {
   EdibleSearchResult toModel(EdibleSearchResultDbModel dbModel) {
     return EdibleSearchResult(
-      id: dbModel.id,
-      name: dbModel.name,
-      description: dbModel.description ?? '',
-      type: dbModel.food_id != null
-          ? EdibleSearchResultType.food
-          : EdibleSearchResultType.dish,
-    );
+        id: dbModel.id,
+        name: dbModel.name,
+        description: dbModel.description ?? '',
+        type: dbModel.food_id != null
+            ? EdibleSearchResultType.food
+            : EdibleSearchResultType.dish,
+        lastEatenAt: dbModel.last_eaten_at != null
+            ? dt.parseISO8601(dbModel.last_eaten_at!)
+            : null);
   }
 }
