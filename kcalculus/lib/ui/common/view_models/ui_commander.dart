3b033338d927992d1d8ef286fbef8e0066d906ce
Viktor Grekov <viktor.grekov@gmail.com>
2025-01-30 16:39:37 -0500

    Adds buffering capability to UiCommander.
    To let it survive brief loss of a listener without loosing commands.
    Primarily, during view re-build triggered by view model re-build.

--- kcalculus/lib/ui/common/view_models/ui_commander.dart
+++ kcalculus/lib/ui/common/view_models/ui_commander.dart
@@ -1,13 +1,29 @@
 import 'dart:async';
+import 'dart:collection';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kcalculus/ui/common/view_models/ui_command.dart';
 
 class UiCommander<Command> {
+  UiCommander([UiCommander<Command>? prev]) {
+    if (prev != null) {
+      _buffer.addAll(prev._buffer);
+    }
+  }
+
   final _commandController = StreamController<UiCommand>();
 
+  final _buffer = Queue<UiCommand>();
+
+  bool _isListenedTo = false;
+
   late final provider = StreamProvider<UiCommand>(
-    (ref) => _commandController.stream,
+    (ref) {
+      ref.onAddListener(_onAddListener);
+      ref.onRemoveListener(_onRemoveListener);
+
+      return _commandController.stream;
+    },
   );
 
   Future<Result> send<Payload, Result>(
@@ -18,11 +34,29 @@ class UiCommander<Command> {
       type,
       payload: payload,
     );
-    _commandController.add(command);
+
+    if (_isListenedTo) {
+      _commandController.add(command);
+    } else {
+      _buffer.add(command);
+    }
+
     return command.future;
   }
 
   void dispose() {
     _commandController.close();
+  }
+
+  void _onAddListener() {
+    _isListenedTo = true;
+
+    while (_buffer.isNotEmpty) {
+      _commandController.add(_buffer.removeFirst());
+    }
+  }
+
+  void _onRemoveListener() {
+    _isListenedTo = false;
   }
 }
