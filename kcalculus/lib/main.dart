8ef2506cb7e4103b75805a1af3d1076e72d87ee1
Viktor Grekov <viktor.grekov@gmail.com>
2025-02-04 18:51:09 -0500

    Sets up Firebase Crashlytics.

--- kcalculus/lib/main.dart
+++ kcalculus/lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kcalculus/firebase_options.dart';
 import 'package:kcalculus/ui/app/widgets/app.dart';
 
 void main() async {
@@ -10,6 +13,19 @@ void main() async {
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+
+  WidgetsBinding.instance.platformDispatcher.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
 
   runApp(const ProviderScope(
     child: App(),
