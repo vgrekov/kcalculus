62631695d361781177696e0574198d3808320436
Viktor Grekov <viktor.grekov@gmail.com>
2025-02-05 11:44:20 -0500

    Sets up logging.

--- kcalculus/lib/main.dart
+++ kcalculus/lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kcalculus/firebase_options.dart';
 import 'package:kcalculus/ui/app/widgets/app.dart';
+import 'package:logging/logging.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,14 +20,28 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  FlutterError.onError = (errorDetails) {
-    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
-  };
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
 
   WidgetsBinding.instance.platformDispatcher.onError = (error, stack) {
     FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
     return true;
   };
+
+  Logger.root.level = kReleaseMode ? Level.INFO : Level.ALL;
+  Logger.root.onRecord.listen((record) {
+    if (record.level >= Level.SEVERE) {
+      FirebaseCrashlytics.instance.recordError(
+        record.error,
+        record.stackTrace,
+        reason: record.toString(),
+        fatal: false,
+      );
+    } else {
+      FirebaseCrashlytics.instance.log(record.toString());
+    }
+
+    debugPrint(record.toString());
+  });
 
   runApp(const ProviderScope(
     child: App(),
