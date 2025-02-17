aa5fd2948827e88d74805fb335408ea794dc1ab5
Viktor Grekov <viktor.grekov@gmail.com>
2025-02-17 18:38:27 -0500

    Cleanup

--- kcalculus/lib/ui/dishes/list/view_models/dish_list_view_model.dart
+++ kcalculus/lib/ui/dishes/list/view_models/dish_list_view_model.dart
@@ -1,14 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kcalculus/data/providers.dart';
-import 'package:kcalculus/domain/models/amount.dart';
-import 'package:kcalculus/domain/models/dish/dish.dart';
-import 'package:kcalculus/domain/models/dish/ingredient.dart';
 import 'package:kcalculus/domain/models/edible_search_result.dart';
-import 'package:kcalculus/domain/models/food.dart';
-import 'package:kcalculus/domain/models/nutrition/nutrient_data.dart';
-import 'package:kcalculus/domain/models/nutrition/nutrition_facts.dart';
-import 'package:kcalculus/domain/models/nutrition/nutrition_ratio.dart';
-import 'package:kcalculus/domain/models/units.dart';
 import 'package:kcalculus/ui/common/view_models/paginator.dart';
 import 'package:kcalculus/ui/common/view_models/search_controller.dart';
 import 'package:kcalculus/ui/common/view_models/ui_command.dart';
@@ -63,45 +55,6 @@ class DishListViewModel extends Notifier<DishListUiState> {
   }
 
   StreamProvider<UiCommand> get commandProvider => _commander!.provider;
-
-  void generateDummyFoodsAndDishes() async {
-    final dishRepo = ref.read(dishRepositoryProvider);
-    for (var i = 50; i > 0; i--) {
-      await dishRepo.save(
-        Dish(
-          name: 'Dummy Dish $i',
-          description: 'For testing pagination',
-          ingredients: [
-            Ingredient(
-              edible: Food(
-                name: 'Dummy Food $i',
-                description: 'For testing pagination',
-                nutritionFacts: [
-                  NutritionFacts(
-                    amount: Amount(unit: Unit.gram, value: 100),
-                    nutrientData: NutrientData(
-                      calories: 100,
-                      fatInGrams: 4,
-                      carbsInGrams: 10,
-                      fiberInGrams: 1,
-                      proteinInGrams: 9,
-                    ),
-                  ),
-                ],
-              ),
-              amount: Amount(unit: Unit.gram, value: 100),
-            ),
-          ],
-          nutritionRatios: {
-            Measure.mass: NutritionRatio(
-              perAmount: Amount(unit: Unit.gram, value: 100),
-              totalAmount: Amount(unit: Unit.gram, value: 100),
-            ),
-          },
-        ),
-      );
-    }
-  }
 
   Future<void> deleteDish(String id) async {
     _log.finer('deleteDish() START');
