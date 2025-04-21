d6a256fdd2b24cf5b4bff56bef1748b6e876faff
Viktor Grekov <viktor.grekov@gmail.com>
2025-04-21 12:45:26 -0400

    Fixes migration script: gets rid of drop column statements.

--- kcalculus/assets/db/migrations/016.sql
+++ kcalculus/assets/db/migrations/016.sql
@@ -1,60 +1,38 @@
-ALTER TABLE dishes
-ADD COLUMN mass_per_amount_value REAL;
+PRAGMA foreign_keys = OFF;
 
 --SQL-STATEMENT-SEPARATOR
 
-ALTER TABLE dishes
-ADD COLUMN mass_per_amount_unit TEXT;
+ALTER TABLE dishes RENAME TO dishes_old;
 
 --SQL-STATEMENT-SEPARATOR
 
-ALTER TABLE dishes
-ADD COLUMN mass_total_amount_value REAL;
+CREATE TABLE dishes (
+    id TEXT NOT NULL PRIMARY KEY,
+    mass_per_amount_value REAL,
+    mass_per_amount_unit TEXT,
+    mass_total_amount_value REAL,
+    mass_total_amount_unit TEXT,
+    volume_per_amount_value REAL,
+    volume_per_amount_unit TEXT,
+    volume_total_amount_value REAL,
+    volume_total_amount_unit TEXT,
+    quantity_per_amount_value REAL,
+    quantity_per_amount_unit TEXT,
+    quantity_total_amount_value REAL,
+    quantity_total_amount_unit TEXT,
+    FOREIGN KEY (id) REFERENCES edibles (id)
+);
 
 --SQL-STATEMENT-SEPARATOR
 
-ALTER TABLE dishes
-ADD COLUMN mass_total_amount_unit TEXT;
-
---SQL-STATEMENT-SEPARATOR
-
-ALTER TABLE dishes
-ADD COLUMN volume_per_amount_value REAL;
-
---SQL-STATEMENT-SEPARATOR
-
-ALTER TABLE dishes
-ADD COLUMN volume_per_amount_unit TEXT;
-
---SQL-STATEMENT-SEPARATOR
-
-ALTER TABLE dishes
-ADD COLUMN volume_total_amount_value REAL;
-
---SQL-STATEMENT-SEPARATOR
-
-ALTER TABLE dishes
-ADD COLUMN volume_total_amount_unit TEXT;
-
---SQL-STATEMENT-SEPARATOR
-
-ALTER TABLE dishes
-ADD COLUMN quantity_per_amount_value REAL;
-
---SQL-STATEMENT-SEPARATOR
-
-ALTER TABLE dishes
-ADD COLUMN quantity_per_amount_unit TEXT;
-
---SQL-STATEMENT-SEPARATOR
-
-ALTER TABLE dishes
-ADD COLUMN quantity_total_amount_value REAL;
-
---SQL-STATEMENT-SEPARATOR
-
-ALTER TABLE dishes
-ADD COLUMN quantity_total_amount_unit TEXT;
+INSERT INTO dishes (
+    id,
+    mass_total_amount_value
+)
+SELECT
+    id,
+    weight_in_grams
+FROM dishes_old;
 
 --SQL-STATEMENT-SEPARATOR
 
@@ -62,10 +40,12 @@ UPDATE dishes
 SET
     mass_per_amount_value = 100,
     mass_per_amount_unit = 'gram',
-    mass_total_amount_value = weight_in_grams,
     mass_total_amount_unit = 'gram';
 
 --SQL-STATEMENT-SEPARATOR
 
-ALTER TABLE dishes
-DROP COLUMN weight_in_grams;
+DROP TABLE dishes_old;
+
+--SQL-STATEMENT-SEPARATOR
+
+PRAGMA foreign_keys = ON;
