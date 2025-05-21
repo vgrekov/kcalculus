690002801ded3729b14906d9bed3b453149e2de2
Viktor Grekov <viktor.grekov@gmail.com>
2025-05-21 18:45:04 -0400

    Implements food container list screen.

---
+++ kcalculus/lib/ui/food_containers/list/view_models/food_container_list_view_model.dart
@@ -0,0 +1,107 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kcalculus/data/providers.dart';
+import 'package:kcalculus/domain/models/food_container.dart';
+import 'package:kcalculus/ui/common/view_models/search/search_helper.dart';
+import 'package:kcalculus/ui/common/view_models/search/search_ui_state.dart';
+import 'package:kcalculus/ui/common/view_models/ui_command.dart';
+import 'package:kcalculus/ui/common/view_models/ui_commander.dart';
+import 'package:kcalculus/ui/food_containers/common/view_models/food_container_search_helper.dart';
+import 'package:kcalculus/utils/logging_analytics.dart';
+import 'package:logging/logging.dart';
+
+final Logger _log = Logger('FoodContainerListViewModel');
+
+enum FoodContainerListCommand {
+  showUnknownErrorNotification,
+  showDeletionSuccessNotification,
+  showDeletionFailureNotification,
+}
+
+class FoodContainerListViewModel
+    extends Notifier<SearchUiState<FoodContainer>> {
+  static const _kPageSize = 25;
+
+  UiCommander<FoodContainerListCommand>? _commander;
+
+  late final SearchHelper<FoodContainer> searchHelper =
+      FoodContainerSearchHelper(
+    pageSize: _kPageSize,
+    getRef: () => ref,
+    getState: () => state,
+    setState: (value) => state = value,
+  );
+
+  @override
+  SearchUiState<FoodContainer> build() {
+    ref.watch(foodContainerRepositoryProvider);
+
+    ref.watch(foodContainerChangesProvider);
+
+    _commander = UiCommander<FoodContainerListCommand>(_commander);
+
+    ref.onDispose(() {
+      _commander?.dispose();
+    });
+
+    String query = stateOrNull?.searchQuery ?? '';
+
+    return searchHelper.initState(query);
+  }
+
+  StreamProvider<UiCommand> get commandProvider => _commander!.provider;
+
+  Future<void> deleteFoodContainer(String id) async {
+    _log.finer('deleteFoodContainer() START');
+
+    try {
+      _log.finest('deleteFoodContainer() Deleting food container with ID: $id');
+
+      final deleted =
+          await ref.read(foodContainerRepositoryProvider).delete(id);
+
+      _log.info('Food container deleted: $deleted');
+      _log.eventFoodContainerDelete();
+
+      if (deleted) {
+        _commander!.send<String, void>(
+          FoodContainerListCommand.showDeletionSuccessNotification,
+          payload: id,
+        );
+      } else {
+        _commander!
+            .send(FoodContainerListCommand.showDeletionFailureNotification);
+      }
+    } catch (error, stackTrace) {
+      _log.severe('Failed to delete food container', error, stackTrace);
+
+      _commander!.send(FoodContainerListCommand.showUnknownErrorNotification);
+    }
+
+    _log.finer('deleteFoodContainer() END');
+  }
+
+  Future<void> restoreFoodContainer(String id) async {
+    _log.finer('restoreFoodContainer() START');
+
+    try {
+      _log.finest('restoreFoodContainer() Restoring food with ID: $id');
+
+      final restored =
+          await ref.read(foodContainerRepositoryProvider).restore(id);
+
+      _log.info('Food container restored: $restored');
+      _log.eventFoodContainerRestore();
+    } catch (error, stackTrace) {
+      _log.severe('Failed to restore food container', error, stackTrace);
+
+      _commander!.send(FoodContainerListCommand.showUnknownErrorNotification);
+    }
+
+    _log.finer('restoreFoodContainer() END');
+  }
+}
+
+final foodContainerListViewModel =
+    NotifierProvider<FoodContainerListViewModel, SearchUiState<FoodContainer>>(
+  () => FoodContainerListViewModel(),
+);
--- kcalculus/lib/ui/more/widgets/containers_grid_item.dart
+++ kcalculus/lib/ui/more/widgets/containers_grid_item.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:kcalculus/ui/food_containers/list/widgets/food_container_list_screen.dart';
 import 'package:kcalculus/ui/more/widgets/more_grid_item.dart';
 import 'package:kcalculus/utils/l10n.dart';
 
 class ContainersGridItem extends StatelessWidget {
   const ContainersGridItem({super.key});
 
+  void _showContainers(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => FoodContainerListScreen(),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MoreGridItem(
-      onTap: () {},
+      onTap: () {
+        _showContainers(context);
+      },
       title: l10n(context).moreItemContainersTitle,
       subtitle: l10n(context).moreItemContainersSubtitle,
       image: Image.asset(
