540ac984740d60bf548f4baecbbef261826f856b
Viktor Grekov <viktor.grekov@gmail.com>
2025-05-21 09:49:29 -0400

    Adds FoodContainer model.

---
+++ kcalculus/lib/domain/models/food_container.dart
@@ -0,0 +1,20 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:kcalculus/domain/models/amount.dart';
+
+part 'food_container.freezed.dart';
+part 'food_container.g.dart';
+
+@Freezed(
+  fromJson: false,
+  toJson: true,
+)
+class FoodContainer with _$FoodContainer {
+  const factory FoodContainer({
+    String? id,
+    required String name,
+    required String description,
+    required Amount weight,
+    DateTime? createdAt,
+    DateTime? updatedAt,
+  }) = _FoodContainer;
+}
