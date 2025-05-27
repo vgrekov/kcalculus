d78aa199909d293ba54afdc269cb760fe9cbbc5f
Viktor Grekov <viktor.grekov@gmail.com>
2025-05-27 11:42:14 -0400

    Adds migration to support dynamic nutrients.

---
+++ kcalculus/assets/db/migrations/020.sql
@@ -0,0 +1,124 @@
+PRAGMA foreign_keys = OFF;
+
+--SQL-STATEMENT-SEPARATOR
+
+ALTER TABLE nutrition_facts RENAME TO nutrition_facts_old;
+
+--SQL-STATEMENT-SEPARATOR
+
+CREATE TABLE nutrition_facts (
+    id TEXT NOT NULL PRIMARY KEY,
+    edible_id TEXT NOT NULL,
+    amount_value REAL NOT NULL,
+    amount_unit TEXT NOT NULL,
+    FOREIGN KEY (edible_id) REFERENCES edibles (id)
+);
+
+--SQL-STATEMENT-SEPARATOR
+
+INSERT INTO nutrition_facts (
+    id,
+    edible_id,
+    amount_value,
+    amount_unit
+)
+SELECT
+    edible_id || '_' || amount_unit,
+    edible_id,
+    amount_value,
+    amount_unit
+FROM nutrition_facts_old;
+
+--SQL-STATEMENT-SEPARATOR
+
+CREATE TABLE nutrient_amounts (
+    nutrition_facts_id TEXT NOT NULL,
+    nutrient TEXT NOT NULL,
+    amount_value REAL NOT NULL,
+    amount_unit TEXT NOT NULL,
+    FOREIGN KEY (nutrition_facts_id) REFERENCES nutrition_facts (id),
+    PRIMARY KEY(nutrition_facts_id, nutrient)
+);
+
+--SQL-STATEMENT-SEPARATOR
+
+INSERT INTO nutrient_amounts (
+    nutrition_facts_id,
+    nutrient,
+    amount_value,
+    amount_unit
+)
+SELECT
+    edible_id || '_' || amount_unit,
+    'energy',
+    calories,
+    'calorie'
+FROM nutrition_facts_old;
+
+--SQL-STATEMENT-SEPARATOR
+
+INSERT INTO nutrient_amounts (
+    nutrition_facts_id,
+    nutrient,
+    amount_value,
+    amount_unit
+)
+SELECT
+    edible_id || '_' || amount_unit,
+    'fat',
+    fat_in_grams,
+    'gram'
+FROM nutrition_facts_old;
+
+--SQL-STATEMENT-SEPARATOR
+
+INSERT INTO nutrient_amounts (
+    nutrition_facts_id,
+    nutrient,
+    amount_value,
+    amount_unit
+)
+SELECT
+    edible_id || '_' || amount_unit,
+    'totalCarbs',
+    carbs_in_grams,
+    'gram'
+FROM nutrition_facts_old;
+
+--SQL-STATEMENT-SEPARATOR
+
+INSERT INTO nutrient_amounts (
+    nutrition_facts_id,
+    nutrient,
+    amount_value,
+    amount_unit
+)
+SELECT
+    edible_id || '_' || amount_unit,
+    'fiber',
+    fiber_in_grams,
+    'gram'
+FROM nutrition_facts_old;
+
+--SQL-STATEMENT-SEPARATOR
+
+INSERT INTO nutrient_amounts (
+    nutrition_facts_id,
+    nutrient,
+    amount_value,
+    amount_unit
+)
+SELECT
+    edible_id || '_' || amount_unit,
+    'protein',
+    protein_in_grams,
+    'gram'
+FROM nutrition_facts_old;
+
+--SQL-STATEMENT-SEPARATOR
+
+DROP TABLE nutrition_facts_old;
+
+--SQL-STATEMENT-SEPARATOR
+
+PRAGMA foreign_keys = ON;
