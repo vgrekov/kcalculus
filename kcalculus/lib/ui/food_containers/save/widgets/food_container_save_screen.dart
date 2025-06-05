681dda9aa4d5d5be4daf2bc98627131f92afa9ea
Viktor Grekov <viktor.grekov@gmail.com>
2025-06-05 19:07:29 -0400

    Removes interstitial ad after food container saving.

--- kcalculus/lib/ui/food_containers/save/widgets/food_container_save_screen.dart
+++ kcalculus/lib/ui/food_containers/save/widgets/food_container_save_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kcalculus/domain/models/food_container.dart';
 import 'package:kcalculus/domain/models/units.dart';
-import 'package:kcalculus/ui/common/utils/ads.dart';
 import 'package:kcalculus/ui/common/utils/messaging/message_type.dart';
 import 'package:kcalculus/ui/common/utils/messaging/state_messenger.dart';
 import 'package:kcalculus/ui/common/utils/progress_overlay.dart';
@@ -181,8 +180,6 @@ class _FoodContainerSaveScreenState
     required BuildContext context,
     required WidgetRef ref,
   }) async {
-    await showInterstitialAd(ref);
-
     _exit();
 
     command?.complete();
