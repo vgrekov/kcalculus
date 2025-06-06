11b480b3502eb0b5deeab3709c63b32eb11c427f
Viktor Grekov <viktor.grekov@gmail.com>
2025-06-06 14:20:53 -0400

    Ads timeout related config options.

--- kcalculus/lib/data/services/app_config/app_config.dart
+++ kcalculus/lib/data/services/app_config/app_config.dart
@@ -1,15 +1,21 @@
 abstract interface class AppConfig {
   String get openFoodFactsBaseUrl;
 
+  int get openFoodFactsTimeoutMillis;
+
   String get contactEmail;
 
   String get androidInterstitialAdUnitId;
 
   String get iOsInterstitialAdUnitId;
 
+  int get interstitialAdTimeoutMillis;
+
   String get androidUnlockAdUnitId;
 
   String get iOsUnlockAdUnitId;
+
+  int get unlockAdTimeoutMillis;
 
   int get unlockWithAdDurationMins;
 
--- kcalculus/lib/data/services/app_config/local/app_config.g.dart
+++ kcalculus/lib/data/services/app_config/local/app_config.g.dart
@@ -9,12 +9,17 @@ part of 'app_config.dart';
 _$LocalAppConfigImpl _$$LocalAppConfigImplFromJson(Map<String, dynamic> json) =>
     _$LocalAppConfigImpl(
       openFoodFactsBaseUrl: json['openFoodFactsBaseUrl'] as String,
+      openFoodFactsTimeoutMillis:
+          (json['openFoodFactsTimeoutMillis'] as num).toInt(),
       contactEmail: json['contactEmail'] as String,
       androidInterstitialAdUnitId:
           json['androidInterstitialAdUnitId'] as String,
       iOsInterstitialAdUnitId: json['iOsInterstitialAdUnitId'] as String,
+      interstitialAdTimeoutMillis:
+          (json['interstitialAdTimeoutMillis'] as num).toInt(),
       androidUnlockAdUnitId: json['androidUnlockAdUnitId'] as String,
       iOsUnlockAdUnitId: json['iOsUnlockAdUnitId'] as String,
+      unlockAdTimeoutMillis: (json['unlockAdTimeoutMillis'] as num).toInt(),
       unlockWithAdDurationMins:
           (json['unlockWithAdDurationMins'] as num).toInt(),
       interstitialAdCooldownDurationMins:
@@ -25,11 +30,14 @@ Map<String, dynamic> _$$LocalAppConfigImplToJson(
         _$LocalAppConfigImpl instance) =>
     <String, dynamic>{
       'openFoodFactsBaseUrl': instance.openFoodFactsBaseUrl,
+      'openFoodFactsTimeoutMillis': instance.openFoodFactsTimeoutMillis,
       'contactEmail': instance.contactEmail,
       'androidInterstitialAdUnitId': instance.androidInterstitialAdUnitId,
       'iOsInterstitialAdUnitId': instance.iOsInterstitialAdUnitId,
+      'interstitialAdTimeoutMillis': instance.interstitialAdTimeoutMillis,
       'androidUnlockAdUnitId': instance.androidUnlockAdUnitId,
       'iOsUnlockAdUnitId': instance.iOsUnlockAdUnitId,
+      'unlockAdTimeoutMillis': instance.unlockAdTimeoutMillis,
       'unlockWithAdDurationMins': instance.unlockWithAdDurationMins,
       'interstitialAdCooldownDurationMins':
           instance.interstitialAdCooldownDurationMins,
