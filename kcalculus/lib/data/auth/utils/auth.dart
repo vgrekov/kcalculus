7d74ea29a5c4542c1d173a341ff1de3f24bc25c3
Viktor Grekov <viktor.grekov@gmail.com>
2025-09-30 15:23:05 -0400

    Improves auth guard functionality.

--- kcalculus/lib/data/auth/utils/auth.dart
+++ kcalculus/lib/data/auth/utils/auth.dart
@@ -2,10 +2,17 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:kcalculus/domain/auth/exceptions/auth_required_exception.dart';
 
 class Auth {
-  static T guard<T>(T Function(User user) fun) {
+  static T guard<T>(
+    T Function(User user) fun, {
+    T Function()? onNotAuthenticated,
+  }) {
     final auth = FirebaseAuth.instance;
     if (auth.currentUser == null) {
-      throw AuthRequiredException();
+      if (onNotAuthenticated != null) {
+        return onNotAuthenticated();
+      } else {
+        throw AuthRequiredException();
+      }
     }
 
     return fun(auth.currentUser!);
