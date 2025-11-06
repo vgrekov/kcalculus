59bc493813d63509102047e4c89f45b9fe3d6ce9
Viktor Grekov <viktor.grekov@gmail.com>
2025-11-06 14:39:23 -0500

    Adds basic constructor args validation for page config.

--- kcalculus/lib/domain/_common/models/page_config.dart
+++ kcalculus/lib/domain/_common/models/page_config.dart
@@ -1,9 +1,18 @@
 class PageConfig<T> {
-  const PageConfig({
+  PageConfig({
     required this.size,
     this.offset,
     this.startAfter,
-  });
+  }) {
+    if (size <= 0) {
+      throw ArgumentError('"size" argument must be a positive integer');
+    }
+
+    if (offset != null && offset! < 0) {
+      throw ArgumentError(
+          'If present, "offset" argument must be a non-negative integer');
+    }
+  }
 
   final int size;
 
