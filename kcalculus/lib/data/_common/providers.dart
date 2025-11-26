e9ffbb86297a49ff3fe629abcb169ef1d045b089
Viktor Grekov <viktor.grekov@gmail.com>
2025-11-26 11:42:34 -0500

    Implements device ID provider.

--- kcalculus/lib/data/_common/providers.dart
+++ kcalculus/lib/data/_common/providers.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kcalculus/domain/_common/models/app_info.dart';
+import 'package:kcalculus/utils/ids.dart';
 import 'package:package_info_plus/package_info_plus.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 final appInfoProvider = FutureProvider<AppInfo>(
   (ref) async {
@@ -11,5 +13,22 @@ final appInfoProvider = FutureProvider<AppInfo>(
       version: packageInfo.version,
       buildNumber: packageInfo.buildNumber,
     );
+  },
+);
+
+const _kDeviceIdPref = "deviceId";
+
+final deviceIdProvider = FutureProvider<String>(
+  (ref) async {
+    final prefs = await SharedPreferences.getInstance();
+
+    String? id = prefs.getString(_kDeviceIdPref);
+
+    if (id == null) {
+      id = generateId();
+      await prefs.setString(_kDeviceIdPref, id);
+    }
+
+    return id;
   },
 );
