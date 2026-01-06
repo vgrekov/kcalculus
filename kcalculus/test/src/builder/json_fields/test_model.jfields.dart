d0e09a17bea4bb91617cf76a1a72179d1bc57a50
Viktor Grekov <viktor.grekov@gmail.com>
2026-01-06 14:06:08 -0500

    Adds JsonFields generator tests.

---
+++ kcalculus/test/src/builder/json_fields/test_model.jfields.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// dart format width=80
+
+// **************************************************************************
+// JsonFieldsGenerator
+// **************************************************************************
+
+part of 'test_model.dart';
+
+class TestModelJsonFields {
+  const TestModelJsonFields._();
+
+  static const id = 'id';
+  static const name = 'name';
+  static const description = 'desc';
+  static const json = 'json';
+}
