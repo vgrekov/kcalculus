fbbb06cb117e31ed5df2036574f9aea7c3708302
Viktor Grekov <viktor.grekov@gmail.com>
2026-01-23 11:43:45 -0500

    Adds missing audit fields in model -> DB mapping for foods and dishes.

--- kcalculus/lib/data/storage/local/food/converters/food_converter.dart
+++ kcalculus/lib/data/storage/local/food/converters/food_converter.dart
@@ -28,6 +28,18 @@ class LocalFoodConverter extends Notifier<void> {
       nf_preview_protein_value: nf?.nutrientData.proteinInGrams,
       nf_preview_fiber_unit: Unit.gram.name,
       nf_preview_fiber_value: nf?.nutrientData.fiberInGrams,
+      created_at: model.createdAt != null
+          ? dt.formatISO8601(model.createdAt!)
+          : null,
+      updated_at: model.updatedAt != null
+          ? dt.formatISO8601(model.updatedAt!)
+          : null,
+      last_eaten_at: model.lastEatenAt != null
+          ? dt.formatISO8601(model.lastEatenAt!)
+          : null,
+      deleted_at: model.deletedAt != null
+          ? dt.formatISO8601(model.deletedAt!)
+          : null,
     );
   }
 
